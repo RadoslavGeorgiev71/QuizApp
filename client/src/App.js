7e8d0862c0b2204f68f4bd7ed276ab56ebe904dd
Radoslav Georgiev <R.K.Georgiev@student.tudelft.nl>
2024-11-27 01:15:40 +0100

    Connected backend and frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {firstFunction} from "./api/FirstService";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [firstText, setFirstText] = useState("")
+  useEffect(() => {
+    firstFunction().then(response => {
+      setFirstText(response.data)
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {firstText}
     </div>
   );
 }
